b10eb4f90dcfec929a56ed370e496f5b8766c6fb
ltbird259 <31287035+ltbird259@users.noreply.github.com>
2019-11-02 11:37:56 -0500

    modified after realizing I misread the problem

--- PyPoll/Main.py
+++ PyPoll/Main.py
@@ -1,1 +1,16 @@
+#pypoll analysis
+#libraries
+import pandas as pd
+#import data
+polls = pd.DataFrame(pd.read_csv("../Instructions/PyPoll/resources/election_data.csv"))
 
+#count of votes
+pollcount = polls["Candidate"].count()
+print(pollcount)
+
+#Candidates who got votes
+candidates = polls["Candidate"].unique()
+print(candidates)
+
+votenums = polls["Candidate"].value_counts()
+print(votenums)
--- Pybank/Main.py
+++ Pybank/Main.py
@@ -6,39 +6,43 @@ basedata = pd.DataFrame(pd.read_csv("../Instructions/PyBank/resources/budget_dat
 
 #total months
 months = basedata["Date"].count()
-print(months)
+# print(months)
 
 #total profit
 totalprofit = basedata["Profit/Losses"].sum()
-print(totalprofit)
+# print(totalprofit)
 
-#average profit
-averageprofit = basedata["Profit/Losses"].mean()
-print(averageprofit)
 
 #declare vars for 'for' loop
 profitdiffgain = 0
 profitdiffloss = 0
 previous = 0
+alldiff = []
 #iterate through records checking to see largest difference
 for record in basedata["Profit/Losses"]:
-#finding largest profit
-    if record - previous > profitdiffgain:
-        profitdiffgain = record - previous
+#define profit difference
+    profitdiff = record - previous
+    #store profit diff
+    alldiff.append(profitdiff)
+    #finding largest profit
+    if profitdiff > profitdiffgain:
+        profitdiffgain = profitdiff
 #finding largest loss
-    elif record - previous < profitdiffloss:
-        profitdiffloss = record - previous
+    elif profitdiff < profitdiffloss:
+        profitdiffloss = profitdiff
     previous = record
+#finding the mean    
+alldiffmean = sum(alldiff)/len(alldiff)
 
-
+#print results
 print(f"""
 
 Financial analysis
 ------------------------------------
 Total months: {months}
 Total profit: ${totalprofit}
-Average profit: ${averageprofit}
 
+Average profit change: ${alldiffmean}
 Greatest increase in profit: ${profitdiffgain}
 Greatest decrease in profit: ${profitdiffloss}
 
