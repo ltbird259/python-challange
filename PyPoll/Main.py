cda7dc9c7eb90dd6307ffe2589ddcf4255aec95a
ltbird259 <31287035+ltbird259@users.noreply.github.com>
2019-11-02 19:42:17 -0500

    update of the project

--- PyPoll/Main.py
+++ PyPoll/Main.py
@@ -12,5 +12,43 @@ print(pollcount)
 candidates = polls["Candidate"].unique()
 print(candidates)
 
-votenums = polls["Candidate"].value_counts()
-print(votenums)
+khan =0
+correy=0
+li=0
+otooley=0
+for vote in polls["Candidate"]:
+    if vote =="Khan":
+        khan += 1
+    elif vote =="Correy":
+        correy += 1
+    elif vote =="Li":
+        li +=1
+    elif vote =="O'Tooley":
+        otooley += 1
+    else:
+        print("problem")
+
+arrayvotes = [khan,correy,li,otooley]
+winner = max(arrayvotes)
+sumvotes = sum(arrayvotes)
+
+khanpercent = (khan/sumvotes) *100
+correypercent = (correy/sumvotes) *100
+lipercent = (li/sumvotes) *100
+otooleypercent = (otooley/sumvotes) *100
+
+
+print(f"""
+Results of poll
+Number of votes cast: {pollcount}
+All candidates who received votes:
+{candidates}
+Vote count and percentages
+Khan: %{khanpercent} - {khan}
+Correy: %{correypercent} - {correy}
+Li: %{lipercent} - {li}
+O'Tooley: %{otooleypercent} - {otooley}
+
+Winner: Khan
+
+""")
